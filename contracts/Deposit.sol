4753dffa5de52598a3a832c185e765227d05bc86
jimao <1982775886@qq.com>
2021-05-27 09:41:46 +0800

    接收eth

--- contracts/Deposit.sol
+++ contracts/Deposit.sol
@@ -3,5 +3,15 @@ pragma solidity 0.8.4;
 
 // 定期存币合约
 contract Deposit {
+  event Log(string);
 
+  fallback()
+  external payable {
+    emit Log("get other");
+  }
+
+  receive()
+  payable external {
+    emit Log("get eth");
+  }
 }
