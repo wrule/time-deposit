8d116f1fb1accb3103c3e2c3bdf9e70320d7e319
jimao <1982775886@qq.com>
2021-05-28 11:27:54 +0800

    添加代币合约

--- migrations/1_deploy.js
+++ migrations/1_deploy.js
@@ -1,5 +1,7 @@
 const Deposit = artifacts.require('Deposit');
+const TestCoin = artifacts.require('TestCoin');
 
 module.exports = (deployer) => {
   deployer.deploy(Deposit);
+  deployer.deploy(TestCoin);
 };
