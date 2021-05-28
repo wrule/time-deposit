039d2739b144372174d29678928142bbc7ddd2e1
jimao <1982775886@qq.com>
2021-05-28 13:02:29 +0800

    添加测试合约

---
+++ contracts/Test.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.4;
+
+// 测试合约
+contract Test {
+  function Send()
+  public pure returns(bool) {
+    return false;
+  }
+}
--- migrations/1_deploy.js
+++ migrations/1_deploy.js
@@ -1,7 +1,9 @@
 const Deposit = artifacts.require('Deposit');
 const TestCoin = artifacts.require('TestCoin');
+const Test = artifacts.require('Test');
 
 module.exports = (deployer) => {
   deployer.deploy(Deposit);
   deployer.deploy(TestCoin);
+  // deployer.deploy(Test);
 };
