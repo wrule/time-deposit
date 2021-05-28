f5cd0986f1ef4d3b6b117ff6844fbcafb6bcf905
jimao <1982775886@qq.com>
2021-05-28 14:14:32 +0800

    测试合约

--- contracts/Test.sol
+++ contracts/Test.sol
@@ -1,10 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.4;
 
+import "./TestCoin.sol";
+
 // 测试合约
 contract Test {
   function Send()
-  public pure returns(bool) {
-    return false;
+  public returns(bool) {
+    TestCoin coin = TestCoin(0x28239aB476ca691312817a092de89E96EaaC20aE);
+    coin.transfer(0x5b798Cb4f7aa23af9856420921A2AA782Ee96840, 1e11);
+    return true;
   }
 }
