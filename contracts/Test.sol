710fb06622de4fa451af2af258054fc8fbf2790a
jimao <1982775886@qq.com>
2021-05-29 08:11:49 +0800

    修改测试和与

--- contracts/Test.sol
+++ contracts/Test.sol
@@ -6,9 +6,7 @@ import "./TestCoin.sol";
 // 测试合约
 contract Test {
   function Send()
-  public returns(bool) {
-    TestCoin coin = TestCoin(0x28239aB476ca691312817a092de89E96EaaC20aE);
-    coin.transfer(0x5b798Cb4f7aa23af9856420921A2AA782Ee96840, 1e11);
+  public pure returns(bool) {
     return true;
   }
 }
